7408cbc646ee190440a175213360e5e58fc1ca0a
vagrant <vagrant@localhost.localdomain>
2020-08-24 09:01:22 +0000

    recaptcha

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -60,15 +60,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super(resource)
   # end
   prepend_before_action :check_captcha, only: [:create]
-  
+
   private
 
-  def check_captcha
-    self.resource = resource_class.new sign_up_params
-    resource.validate
-    unless verify_recaptcha(model: resource)
-      respond_with_navigational(resource) { render :new }
+    def check_captcha
+      self.resource = resource_class.new sign_up_params
+      resource.validate
+      unless verify_recaptcha(model: resource)
+        respond_with_navigational(resource) { render :new }
+      end
     end
-  end
-  
 end
