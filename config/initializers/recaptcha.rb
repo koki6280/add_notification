5d64f97dbc40d792ec295cb8ecb55612e4e5126f
vagrant <vagrant@localhost.localdomain>
2020-08-25 12:25:44 +0000

    recapycha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.site_key = ENV['6LccjrwZAAAAAHCYmK6XJKqDjlhvS0J49oWSqQ4Q']
-  config.secret_key = ENV['6LccjrwZAAAAAC3JybZQGT90gblz021ZKepCqm3q']
+  config.site_key = '6LccjrwZAAAAAHCYmK6XJKqDjlhvS0J49oWSqQ4Q'
+  config.secret_key = '6LccjrwZAAAAAC3JybZQGT90gblz021ZKepCqm3q'
 end
