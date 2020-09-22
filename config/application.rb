c1e5e306bc1922ef9ee4643deea4e887476404ce
vagrant <K-h.too.brag.is.idiot@docomo.ne.jp>
2020-09-22 12:51:59 +0000

    error_message

--- config/application.rb
+++ config/application.rb
@@ -12,10 +12,21 @@ module QOH
     config.load_defaults 5.2
     config.paths.add 'lib', eager_load: true
     config.i18n.default_locale = :ja
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
     config.time_zone = 'Tokyo'
+
+    config.action_view.field_error_proc = Proc.new do |html_tag, instance|
+        if instance.kind_of?(ActionView::Helpers::Tags::Label)
+            html_tag.html_safe
+        else
+            class_name = instance.object.class.name.underscore
+            method_name = instance.instance_variable_get(:@method_name)
+            "<div class=\"has-error\">#{html_tag}
+              <span class=\"help-block\">
+                #{I18n.t("activerecord.attributes.#{class_name}.#{method_name}")}
+                #{instance.error_message.first}
+              </span>
+            </div>".html_safe
+        end
+    end
   end
 end
